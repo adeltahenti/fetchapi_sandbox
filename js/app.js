a97e1c00ece0a93260d76d78159d1f87ab7c7c6d
adeltahenti <adel.tahenti@citycable.ch>
2018-08-30 08:16:43 +0200

    Fetch -> Get JSON Data

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /**
- * BUtton 1 event listener
+ * Button 1 event listener - Get local text file data
  */
 document.getElementById('button1').addEventListener('click', getText);
 
@@ -20,6 +20,53 @@ function getText() {
           </div>
         </div>
       `;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+/**
+ * Button 2 event listener - Get local JSON data
+ */
+document.getElementById('button2').addEventListener('click', getJSON);
+
+function getJSON() {
+  fetch('../posts.json')
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      // Bootstrap Card - begin
+      let output = `
+        <div class="card">
+          <div class="card-header">
+            JSON File Data
+          </div>
+          <ul class="list-group list-group-flush">
+      `;
+
+      // Bootstrap Card - body
+      data.forEach(post => {
+        output += `
+          <li class="list-group-item">
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Title: </strong>${post.title}</li>
+              <li class="list-group-item"><strong>Body: </strong>${post.body}</li>
+            </ul>
+          </li>
+        `;
+      });
+
+      // Bootstrap Card - end
+      output += `
+          </ul>
+        </div>
+      `;
+      // Output
+      document.getElementById('output').innerHTML = output;
     })
     .catch(function (err) {
       console.log(err);
